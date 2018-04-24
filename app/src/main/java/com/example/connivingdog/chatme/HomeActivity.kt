e7172187a40bfe5fe154b6ed53f056d0123344ca
Ronan Lina <ronanlina@gmail.com>
2018-04-24 16:32:50 +0800

    - attempting to fetch user list

--- app/src/main/java/com/example/connivingdog/chatme/HomeActivity.kt
+++ app/src/main/java/com/example/connivingdog/chatme/HomeActivity.kt
@@ -1,12 +1,46 @@
 package com.example.connivingdog.chatme
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.Toast
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private var mDatabaseReference: DatabaseReference = FirebaseDatabase.getInstance().reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        var email: String = getEmail()
+
+        try {
+            mDatabaseReference.child("users").orderByChild("email").equalTo(email)
+            mDatabaseReference.addValueEventListener( object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.children.forEach{
+                        var userAccount: UserAccount = it.getValue(UserAccount::class.java)!!
+                        var email: String = userAccount.email
+                        var mAdapter = UserListAdapter(mDatabaseReference, this@HomeActivity,email)
+                        userMessageList.adapter = mAdapter
+                    }
+                }
+                override fun onCancelled(databaseError: DatabaseError) {
+                    println("loadPost:onCancelled ${databaseError.toException()}")
+                }
+            })
+        }
+        catch (e: NullPointerException){
+            Toast.makeText(this,"NULL",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getEmail(): String{
+        var intent: Intent = getIntent()
+            return intent.getStringExtra("email")
     }
 }
---
+++ app/src/main/java/com/example/connivingdog/chatme/UserListAdapter.java
@@ -0,0 +1,109 @@
+package com.example.connivingdog.chatme;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+
+public class UserListAdapter extends BaseAdapter{
+
+    private DatabaseReference mDatabaseReference;
+    private Activity mActivity;
+    private ArrayList<DataSnapshot> mSnapshotList;
+    private Query query;
+
+    private ChildEventListener mListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
+
+    public UserListAdapter(DatabaseReference databaseReference, Activity activity, String currentUserEmail) {
+        mActivity = activity;
+        mDatabaseReference = databaseReference.child("users");
+        query = mDatabaseReference.orderByChild("username").equalTo(currentUserEmail);
+        query.addChildEventListener(mListener);
+        mSnapshotList = new ArrayList<>();
+    }
+
+    static class ViewHolder{
+        TextView userText;
+        TextView emailText;
+        LinearLayout.LayoutParams params;
+    }
+
+    @Override
+    public int getCount() {
+        return mSnapshotList.size();
+    }
+
+    @Override
+    public UserAccount getItem(int i) {
+        DataSnapshot snapshot = mSnapshotList.get(i);
+
+        return snapshot.getValue(UserAccount.class);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if(view == null){
+            LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.user_list, viewGroup, false);
+
+            final ViewHolder holder = new ViewHolder();
+            holder.userText = (TextView) view.findViewById(R.id.userText);
+            holder.emailText = (TextView) view.findViewById(R.id.emailText);
+            holder.params = (LinearLayout.LayoutParams) holder.userText.getLayoutParams();
+            view.setTag(holder);
+        }
+
+        final UserAccount account = getItem(i);
+        final ViewHolder holder = (ViewHolder) view.getTag();
+
+        String username = account.getUsername();
+        String email = account.getEmail();
+
+        holder.userText.setText(username);
+        holder.emailText.setText(email);
+        return view;
+    }
+}
